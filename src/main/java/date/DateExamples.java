66c69b5d98fbe3733374c05654e163b8da7a4042
mucheniski <mucheniski@gmail.com>
2021-05-06 15:28:30 -0300

    Exemplos de SimpleDateFormat

--- src/main/java/date/DateExamples.java
+++ src/main/java/date/DateExamples.java
@@ -7,8 +7,10 @@ import java.util.Date;
 public class DateExamples {
 	
 	// https://www.geeksforgeeks.org/date-class-java-examples/
-	private static SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");;
-	
+	private static SimpleDateFormat diaMesAnoFormater = new SimpleDateFormat("dd/MM/yyyy");
+	private static SimpleDateFormat horaMinutoFormater = new SimpleDateFormat("HH:mm");
+	private static SimpleDateFormat diaFormater = new SimpleDateFormat("E");
+
 	public static void main(String[] args) {
 
 		try {
@@ -16,6 +18,10 @@ public class DateExamples {
             String valorData = "2021-04-13T18:40:53.199Z";
             Date data = DateConversor.convertUTCToDate(valorData);
             System.out.println("Data " + data);
+            System.out.println(diaMesAnoFormater.format(data));
+            System.out.println(horaMinutoFormater.format(data));
+            System.out.println(diaFormater.format(data));
+
 
 		} catch (ParseException e) {
 			System.out.println("Exception; " + e);
@@ -25,9 +31,9 @@ public class DateExamples {
 	
     public static void exemplosDateBasicos() throws ParseException  { 
         // Creating date 
-        Date d1 = dateFormatter.parse("27/12/1986");
+        Date d1 = diaMesAnoFormater.parse("27/12/1986");
         Date dataAtual = new Date();  // Current date 
-        Date d3 = dateFormatter.parse("30/03/1991"); 
+        Date d3 = diaMesAnoFormater.parse("30/03/1991");
         
         System.out.println("DATA ATUAL: " + dataAtual);
   
@@ -48,7 +54,6 @@ public class DateExamples {
         System.out.println("Before setting "+dataAtual); 
         dataAtual.setTime(204587433443L); 
         System.out.println("After setting "+dataAtual); 
-    } 
-	
+    }
 
 }
